b620974efdf1a18d22cd1d3e0d019c54558c0cbb
Homarus23 <rstanley@mun.ca>
2017-06-06 10:17:18 -0300

    updated function

--- CartDistFunction.R
+++ CartDistFunction.R
@@ -3,21 +3,14 @@
 #Then plots a map of points, a linear model of least-cost geographic vs Cartesian distances, and adds Cartesian coordinates to your input file.
 #If any sites are too close to land it will stop, as it requires depths <0 metres. 
 
-coord_cartesian<-function(coordinates,min.depth,max.depth,gridres,directory){
+coord_cartesian<-function(coordinates,min.depth,max.depth,trans=NA,gridres,directory){
   
   #gridres = resolution (mins) of the bathymetric grid used for least coast path (default = 2)
   ##Coordinates file must have column names Lat and Long, and column 1 should be your pop names or codes
   
   require(gdistance)
-  require(maps)
-  require(mapdata)
-  require(maptools)
-  require(marmap)
   require(ggplot2)
-  require(dplyr)
-  require(data.table)
-  require(vegan)
-  
+ 
   coords<-read.csv(coordinates,header=T)
   
   if (length(colnames(coords))<3){
@@ -40,38 +33,54 @@ coord_cartesian<-function(coordinates,min.depth,max.depth,gridres,directory){
              "lightsteelblue2", "lightsteelblue1")
   greys <- "grey40"
   
-  png("MyMap.png",width=950,height=600)
-  plot(bathydata,image = TRUE, land = T, lwd = 0.03,
-       bpal = list(c(0, max(bathydata), greys),
-                   c(min(bathydata), 0, blues)))
-  plot(bathydata, lwd = 1, deep = 0, shallow = 0, step = 0, add = TRUE)
-  map("worldHires", xlim=Long.lim, ylim=Lat.lim, col="grey30", fill=TRUE, resolution=0,add=T)
-  #map.axes(cex.axis=2);map.scale(ratio=FALSE)
-  points(coords$Long, coords$Lat,pch=19,cex=2,col="red")
+  #Get depths and plot. If any depths > 0 we will not proceed
+  depths<-marmap::get.depth(bathydata,x=coords$Long,y=coords$Lat,locator=F)
+  
+  #colours to assign those locations which are in water "green" and on land "red". 
+  depths$col <- "green"
+  depths[depths$depth>=0,"col"] <- "red"
+  
+  
+  pdf("MyMap.pdf")
+  
+      marmap::plot.bathy(bathydata,image = TRUE, land = T, lwd = 0.03,
+                         bpal = list(c(0, max(bathydata), greys),
+                                     c(min(bathydata), 0, blues)),deep=0,shallow=0)
+      
+      marmap::plot.bathy(bathydata, lwd = 1, deep = 0, shallow = 0, step = 0, add = TRUE)
+      
+      legend("bottomright",
+             legend = c("Water","Land"), 
+             col=c("green","red"),
+             pch=19,
+             pt.cex=1.5,
+             bg="white")
+      
+      points(depths$lon, depths$lat,pch=19,cex=2,col=depths$col)
+  
   dev.off()
-  
-  #Get depths and if any depths > 0 we will not proceed
-  writeLines("\nMaking sure that all depths are <-1m deep\n")
-  depths<-get.depth(bathydata,x=coords$Long,y=coords$Lat,locator=F)
-  
 
-  for(i in 1:length(depths$depth)){
-    if(depths$depth[i] > 0){
+  
+  if(sum(depths$depth >= 0)>0){
       stop("\nSome of your points appear to be too close to land. Suggest moving points farther off land for this analysis\n\n\n")
     }
-  }
   
+  #if all points are in water then continue
   writeLines("\nAll coordinates appear to be in water.\n")
   
   writeLines("\nCalculating transition object for least-cost analysis.\n")
   
   #Make the trans mat object then do the lc dist calculation
-  trans1 <- trans.mat(bathydata,min.depth = min.depth,max.depth = max.depth) 
+  
+  if(is.na(trans)){
+  trans <- marmap::trans.mat(bathydata,min.depth = min.depth,max.depth = max.depth) 
+  }
+  
   sites<-coords[,c("Long","Lat")]
   rownames(sites)<-coords[,1]
   
   writeLines("Calculating least cost distances. This will probably can take a few minutes depending onresolution...")
-  lc.dists <- lc.dist(trans1, 
+  lc.dists <- marmap::lc.dist(trans, 
                       sites, 
                       res="dist")
   writeLines("Meta MDS scaling into Cartesian coordinates\n\n")
@@ -79,7 +88,7 @@ coord_cartesian<-function(coordinates,min.depth,max.depth,gridres,directory){
   set.seed(1)
   cart.dists <- as.data.frame(metaMDS(lc.dists,k=2)$points) #K=2 because we want 2 dimensions
   set.seed(1)
-  stress.values <- metaMDS(lc.dists,k=2)$stress # this will vary slightly each time.
+  stress.values <- vegan::metaMDS(lc.dists,k=2)$stress # this will vary slightly each time.
   dist.cart.dists <- dist(cart.dists)
   
   if(stress.values>0.05){
@@ -106,9 +115,14 @@ coord_cartesian<-function(coordinates,min.depth,max.depth,gridres,directory){
   #summary(mod)
   
   output <- list(Coords <- cbind(coords,cart.dists),
-                 fitplot <- p1)
+                 fitplot <- p1,
+                 mod <- mod,
+                 trans <- trans,
+                 lc.dist <- lc.dist)
   
   return(output)
+  
+  writeLines(paste0("Writing coordinates to file ",directory,"MyCartesianCoordinates.csv"))
   
   write.csv(x = Coords,file = paste0(directory,"MyCartesianCoordinates.csv"),quote = FALSE,row.names = F)
   
